f6412d0302068c9ea6fe747ea7c3cfd60398c14b
Hamza Oubaha <mahzaboha@gmail.com>
2026-07-16 23:36:33 +0100

    Fix scroll hijacking: add overlay above spline-viewer to block its internal event listeners

--- src/components/layout/SplineBackground.jsx
+++ src/components/layout/SplineBackground.jsx
@@ -3,10 +3,12 @@ import './SplineBackground.css';
 const SplineBackground = () => {
   return (
     <div className="spline-bg">
-      <spline-viewer 
-        url="https://prod.spline.design/eT-FhUCAE2UZWE1b/scene.splinecode" 
+      <spline-viewer
+        url="https://prod.spline.design/eT-FhUCAE2UZWE1b/scene.splinecode"
         events-target="global"
       ></spline-viewer>
+      {/* Transparent overlay to block Spline's internal scroll/pointer listeners */}
+      <div className="spline-bg__overlay" />
     </div>
   );
 };
